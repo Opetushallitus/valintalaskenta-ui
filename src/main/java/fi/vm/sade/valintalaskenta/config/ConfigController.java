4c3b952f6fc9ec5682942fd38ca2b5e118cd9a1b
kkammone <kari.kammonen@gofore.com>
2013-04-26 15:13:06 +0300

    siistitty koodia

--- src/main/java/fi/vm/sade/valintalaskenta/config/ConfigController.java
+++ src/main/java/fi/vm/sade/valintalaskenta/config/ConfigController.java
@@ -5,12 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.servlet.http.HttpServletResponse;
 
 /**
- * User: tommiha
- * Date: 2/15/13
- * Time: 2:43 PM
+ * @author kkammone
  */
 @Controller
 public class ConfigController {
@@ -30,16 +27,13 @@ public class ConfigController {
     @Value("${valintalaskenta-ui.valintalaskenta-service-url.rest}")
     private String valintalaskentaServiceUrl;
 
-
     @Value("${valintalaskenta-ui.sijoittelu-service-url.rest}")
     private String sijoitteluServiceUrl;
 
 
     @RequestMapping(value="/configuration.js", method = RequestMethod.GET, produces="text/javascript")
     @ResponseBody
-    public String index(HttpServletResponse response) {
-       // response.setContentType("text/javascript");
-      //  response.setCharacterEncoding("UTF-8");
+    public String index() {
         StringBuilder b = new StringBuilder();
         append(b, "TARJONTA_URL_BASE", tarjontaServiceUrl);
         append(b, "VALINTAPERUSTEET_URL_BASE ", valintaperusteetServiceUrl);
@@ -48,21 +42,8 @@ public class ConfigController {
         append(b, "SIJOITTELU_URL_BASE", sijoitteluServiceUrl);
         append(b, "SERVICE_URL_BASE", valintalaskentaServiceUrl);
         append(b, "TEMPLATE_URL_BASE", "");
-        /*
-        try{
-            PrintWriter out = response.getWriter();
-            out.println(b.toString());
-            out.close();
-        }
-        catch(IOException ex){
-          ex.printStackTrace();
-        }
-            //response.getWriter().print(b.toString());
-            //return b.toString();
-            */
         return b.toString();
-
-        }
+    }
 
     private void append(StringBuilder b, String key, String value) {
         b.append(key);
