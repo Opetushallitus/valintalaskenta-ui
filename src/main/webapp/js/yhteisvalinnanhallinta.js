34ae472c178ac4e884f2ab87ffe5072595787e42
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2013-05-13 15:46:13 +0300

    turhien lokitusten poistoa ja valinnanhallinnan kälin siistintää

--- src/main/webapp/js/yhteisvalinnanhallinta.js
+++ src/main/webapp/js/yhteisvalinnanhallinta.js
@@ -3,9 +3,6 @@
 function YhteisvalinnanHallintaController($scope, $location, $routeParams, Sijoitteluktivointi, HakuModel  ) {
 	$scope.hakumodel = HakuModel;
 
-	console.log($scope.hakumodel);
-
-
     $scope.kaynnistaSijoittelu = function() {
         var hakuoid = $routeParams.hakuOid;
             Sijoitteluktivointi.aktivoi({hakuOid: hakuoid}, function() {
