6c895ae393250fda463ff9a677bc067f6006da1e
jussijartamo <jussi@jartamo@gofore.com>
2013-07-15 13:42:07 +0300

    VT-488 : Laskennan yhteydessä kerätään historiaa laskennan kulusta. historia on nähtävissä valintalaskenta-ui:ssa.

--- src/main/webapp/js/valintalaskentatulos.js
+++ src/main/webapp/js/valintalaskentatulos.js
@@ -37,6 +37,52 @@ function ValintalaskentatulosController($scope, $location, $routeParams, Valinta
     $scope.model.refresh($scope.hakukohdeOid);
     $scope.valintalaskentatulosExcelExport = SERVICE_EXCEL_URL_BASE + "export/valintalaskentatulos.xls?hakukohdeOid=" + $routeParams.hakukohdeOid;
     
+    $scope.showHistory = function(msg) {
+    	// historia on taulukko merkkijonoja. konvertoidaan jsoniksi
+    	var historiat = [];
+    	angular.forEach(msg, function(historiaString){
+    		historiat.push(angular.fromJson(historiaString));          
+    	});
+    	
+    	function to_ul(historiat) {
+    	    var ul = document.createElement("ul");
+
+    	  for (var i=0, n=historiat.length; i<n; i++) {
+    	      var historia = historiat[i];
+    	      var li = document.createElement("li");
+
+    	        var text = document.createTextNode(historia.funktio+ " ");
+    	        
+    	        var tulos = document.createElement("strong");
+    	        tulos.appendChild(document.createTextNode(" "+historia.tulos));
+    	        
+    	        li.appendChild(text );
+    	        
+    	        angular.forEach(historia.avaimet, function(arvo,avain){
+    	        	var span = document.createElement("span");
+    	        	$(span).attr("style", "color:red;");
+        	        span.appendChild(document.createTextNode(" " + avain + "=" + arvo + " "));
+        	        li.appendChild(span);    
+    	    	});
+    	        
+    	        
+    	        
+    	        li.appendChild(tulos );
+    	        if (historia.historiat) {
+    	             li.appendChild(to_ul(historia.historiat));
+    	        }
+
+    	        ul.appendChild(li);
+    	    }
+
+    	    return ul;
+    	}
+    	//
+    	
+    	var my_window = window.open("", "Historia", "status=1,width=1600,height=900");
+        my_window.document.write($(to_ul(historiat)).html());
+        //alert(msg);
+   };
     /*
     $scope.updateJarjestyskriteerinArvo = function(valintatapajonoOid, hakemusOid, jarjestyskriteeriprioriteetti, kriteerinArvo) {
     	$scope.model.updateJarjestyskriteerinArvo(valintatapajonoOid, hakemusOid, jarjestyskriteeriprioriteetti, kriteerinArvo);
