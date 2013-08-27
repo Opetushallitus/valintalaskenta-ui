f7b29dec0f8534f3351350103af7370d864aa76d
Tapio Jaakkola <tapio.jaakkola@git.mail.kapsi.fi>
2013-08-27 16:05:55 +0300

    VT-504: henkilötiedot näkymän edistämistä

--- src/main/webapp/js/henkilotiedot.js
+++ src/main/webapp/js/henkilotiedot.js
@@ -1,8 +1,13 @@
 
-app.factory('HenkiloTiedotModel', function(Hakemus, ValintalaskentaHakemus, ValintalaskentaHakemus) {
+app.factory('HenkiloTiedotModel', function(Hakemus, ValintalaskentaHakemus, ValintalaskentaHakemus, HakukohdeNimi, ValinnanvaiheListFromValintaperusteet, HakukohdeValinnanvaihe) {
 	var model = new function() {
 		this.hakemus = {};
-		this.haku = {};
+		this.valintalaskentaHakemus = {};
+		
+		this.selectedValinnanvaihe = {};
+		this.selectedHakukohde = {};
+		this.selectedValintatapajono = {};
+
 		this.errors = [];
 
 		this.refresh = function(hakuOid, hakemusOid) {
@@ -10,28 +15,71 @@ app.factory('HenkiloTiedotModel', function(Hakemus, ValintalaskentaHakemus, Vali
 			
 			Hakemus.get({oid: hakemusOid}, function(result) {
 				model.hakemus = result;
+				
+			}, function(error) {
+				model.errors.push(error);
 			});
 
 			ValintalaskentaHakemus.get({hakuoid: hakuOid, hakemusoid: hakemusOid}, function(result) {
-				model.haku = result;
+				model.valintalaskentaHakemus = result;
+				console.log(model.valintalaskentaHakemus);
+				//oletetaan, että valintalaskentahakemukseen liitty aina vain yksi hakukohde
+				if(result.hakukohteet.length > 0) {
+					
+					
+					HakukohdeNimi.get({hakukohdeoid: model.valintalaskentaHakemus.hakukohteet[0].hakukohdeoid}, function(result) {
+						model.selectedHakukohde = result;
+					}, function(error) {
+						model.errors.push(error);
+					});
+
+					ValinnanvaiheListFromValintaperusteet.get({hakukohdeoid: model.valintalaskentaHakemus.hakukohteet[0].hakukohdeoid}, function(result) {
+						var matchingValinnanvaihe = findMatchingValinnanvaihe(result, model.valintalaskentaHakemus.hakukohteet[0].valinnanvaihe[0].valinnanvaiheoid);
+						model.selectedValinnanvaihe = matchingValinnanvaihe;
+					}, function(error) {
+						model.errors.push(error);
+					});
+				
+
+				} else {
+					model.selectedHakukohde = {};
+					model.selectedValinnanvaihe = {};
+					model.selectedValintatapajono = {};
+				}
+
+				
+			}, function(error) {
+				model.errors.push(error);
 			});
+
+
 		}
 
 		this.refreshIfNeeded = function(hakuOid, hakemusOid) {
-			if(model.hakemus.oid !== hakemusOid || model.haku.oid !== hakemusOid) {
+			if(model.hakemus.oid !== hakemusOid || model.valintalaskentaHakemus.hakuoid !== hakemusOid) {
 				model.refresh(hakuOid, hakemusOid);
 			}
+		}
+
+		function findMatchingValinnanvaihe(list, valinnanvaiheoid) {
+
+			var result = {};
+			list.forEach(function(element) {
+				if(element.oid === valinnanvaiheoid) {
+					result = element;
+				}
+			});
+			return result;
 		}
 	}
 	return model;
 });
 
-
-
 function HenkiloTiedotController($scope,$routeParams,HenkiloTiedotModel) {
 	$scope.model = HenkiloTiedotModel;
 
 	$scope.model.refreshIfNeeded($routeParams.hakuOid, $routeParams.hakemusOid);
+	
 	
 
 }
