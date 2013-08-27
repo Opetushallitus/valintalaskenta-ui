3344326e045a52d4226880c36894a06a8361fda4
Juho Kaistinen <jukais@gofore.com>
2013-08-27 14:38:16 +0300

    VT-514, VT-515: virheraportti

--- src/main/webapp/js/yhteisvalinnanhallinta.js
+++ src/main/webapp/js/yhteisvalinnanhallinta.js
@@ -1,6 +1,8 @@
 ﻿
 
 function YhteisvalinnanHallintaController($scope, $location, $routeParams, Sijoitteluktivointi, HakuModel, AktivoiHaunValintalaskenta, ParametriService, HakuVirheet) {
+	$scope.HAKEMUS_UI_URL_BASE = HAKEMUS_UI_URL_BASE;
+
 	$scope.hakumodel = HakuModel;
 
     $scope.kaynnistaSijoittelu = function() {
@@ -18,14 +20,13 @@ function YhteisvalinnanHallintaController($scope, $location, $routeParams, Sijoi
 
     $scope.showErros = function() {
         HakuVirheet.get({parentOid: $routeParams.hakuOid}, function(result) {
-            $scope.virheet = result;
+            if(result.length > 0) {
+                $scope.virheet = result;
+            } else {
+                $scope.eiVirheita = true;
+            }
         });
     }
-
-    HakuVirheet.get({parentOid: $routeParams.hakuOid}, function(result) {
-        $scope.virheet = result;
-    });
-
 
     $scope.showHakukohde = function(virhe) {
         virhe.showValinnanvaihe =! virhe.showValinnanvaihe;
@@ -33,6 +34,10 @@ function YhteisvalinnanHallintaController($scope, $location, $routeParams, Sijoi
 
     $scope.stopPropagination = function($event) {
         $event.stopPropagation();
+    }
+
+    $scope.henkilonakyma = function(hakemusOid) {
+        $location.path('/haku/' + $routeParams.hakuOid + '/henkiloittain/' + hakemusOid + '/henkilotiedot');
     }
 
     $scope.privileges = ParametriService;
