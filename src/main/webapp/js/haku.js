5a04f3e44ce6d81dcb519a0c5b4214f2e1261178
Tapio Jaakkola <tapio.jaakkola@git.mail.kapsi.fi>
2013-09-03 14:43:58 +0300

    lisähaku-tyyppisien hakujen hakukohteille erillinen tabivalikko

--- src/main/webapp/js/haku.js
+++ src/main/webapp/js/haku.js
@@ -3,6 +3,7 @@ app.factory('HakuModel', function($q, Haku, HaunTiedot) {
     model = new function() {
         this.hakuOid;
         this.haut = [];
+        this.lisahaku = false;
 
         this.init = function(oid) {
             
@@ -24,17 +25,12 @@ app.factory('HakuModel', function($q, Haku, HaunTiedot) {
                             },function() {
                                 deferred.resolve();
                             });
-
                             return deferred.promise;
                         })();
                     });
                         
-                    
-
                     //wait until all hakuobjects have been fetched
                     $q.all(promises).then(function() {
-
-
                         //set the previously selected haku or first in list
                         model.hakuOid = model.haut[0].oid;
                         model.haut.forEach(function(haku){
@@ -46,7 +42,6 @@ app.factory('HakuModel', function($q, Haku, HaunTiedot) {
                 });
             }
         };
-
     };
 
 
@@ -55,12 +50,34 @@ app.factory('HakuModel', function($q, Haku, HaunTiedot) {
 
 function HakuController($scope, $location, $routeParams, HakuModel) {
     $scope.model = HakuModel;
-
+    
     HakuModel.init($routeParams.hakuOid);
     $scope.$watch('model.hakuOid', function() {
+        
+        // set lisahaku-flag to true if selected haku is of type lisahaku
+        if($scope.model.hakuOid && isLisahaku($scope.model.hakuOid.oid)) {
+            $scope.model.lisahaku = true;
+        } else {
+            $scope.model.lisahaku = false;
+        }
+        
         if($scope.model.hakuOid && $scope.model.hakuOid.oid != $routeParams.hakuOid) {
             $location.path('/haku/' + HakuModel.hakuOid.oid + '/hakukohde/');
         }
     });
+
+    //return true if selected haku is lisahaku, otherwise false
+    function isLisahaku(hakuOid) {
+        var showLisahakuView = false;
+        $scope.model.haut.forEach(function(haku) {
+            if(hakuOid === haku.oid) { 
+                var hakutyyppi = haku.hakutyyppiUri;
+                var lisahakutyyppiRegExp = /(hakutyyppi_03).*/;
+                var match = lisahakutyyppiRegExp.exec(hakutyyppi);
+                match ? showLisahakuView = true : showLisahakuView = false;
+            }
+        });
+        return showLisahakuView;
+    }
     
 }
