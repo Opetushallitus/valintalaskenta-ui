0ab83b4a41fdb9fc1c14c6fe92c6a662c341ad22
Juho Kaistinen <jukais@gofore.com>
2014-02-04 14:21:13 +0200

    VT-789: Näytetään viimeisin valinnanvaihe ensimmäisenä

--- src/main/webapp/app/haku/hakukohteet/valintalaskenta_tulos/valintalaskentatulos.js
+++ src/main/webapp/app/haku/hakukohteet/valintalaskenta_tulos/valintalaskentatulos.js
@@ -1,26 +1,4 @@
-﻿function AccordionDemoCtrl($scope) {
-    $scope.oneAtATime = true;
-
-    $scope.groups = [
-        {
-            title: "Dynamic Group Header - 1",
-            content: "Dynamic Group Body - 1"
-        },
-        {
-            title: "Dynamic Group Header - 2",
-            content: "Dynamic Group Body - 2"
-        }
-    ];
-
-    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-    $scope.addItem = function() {
-        var newItemNo = $scope.items.length + 1;
-        $scope.items.push('Item ' + newItemNo);
-    };
-}
-
-app.factory('ValintalaskentatulosModel', function(ValinnanvaiheListByHakukohde, JarjestyskriteeriMuokattuJonosija) {
+﻿app.factory('ValintalaskentatulosModel', function(ValinnanvaiheListByHakukohde, JarjestyskriteeriMuokattuJonosija) {
 	var model;
 	model = new function() {
 
