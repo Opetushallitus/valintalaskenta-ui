3c954124507d8332302b5cee653b3ff5d2222bd3
jussijartamo <jussi@jartamo@gofore.com>
2014-02-18 10:38:09 +0200

    VT-787 virheilmoitus jos valintakoetta ei löydy palvelimelta

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -440,6 +440,12 @@ app.factory('Valintakoetulokset', function($resource) {
     });
 });
 
+app.factory('Valintakoe', function($resource) {
+    return $resource(VALINTAPERUSTEET_URL_BASE + "resources/valintakoe/:valintakoeOid", {valintakoeOid: "@valintakoeOid"}, {
+        get: {method: "GET", isArray: true}
+    });
+});
+
 app.factory('ValintakoetuloksetHakemuksittain', function($resource) {
     return $resource(SERVICE_URL_BASE + "resources/valintakoe/hakemus/:hakemusOid", {hakemusOid: "@hakemusOid"}, {
         get: {method: "GET"}
