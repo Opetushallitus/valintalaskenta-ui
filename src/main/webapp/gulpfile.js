0d9011ddd3fd9b49cc4a191b1277e82c6fd98008
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-02-21 14:18:17 +0200

    fronttikirjastot haetaan browserify.js:llä

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -1,22 +1,32 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    browserify = require('gulp-browserify');
 
-var jslibs = [
-	"common/jslib/jquery-2.1.0/jquery-2.1.0.min.js",
+//preinstalled assets not found from npm-packages
+var staticLibs = [
 	"common/jslib/jquery-1.8.3/jquery.nestable.js",
-	"common/jslib/jquery-ui-1.9.2/jquery-ui.min.js",
 	"common/jslib/tinymce-4.0.12/tinymce.min.js",
-	"common/jslib/angular-1.2.10/angular.min.js",
-	"common/jslib/angular-1.2.10/angular-resource.min.js",
-	"common/jslib/angular-1.2.10/angular-route.min.js",
-	"common/jslib/angular-1.2.10/angular-animate.min.js",
-	"common/jslib/tinymce-4.0.12/ui-tinymce.js",
-	"common/jslib/underscore/underscore-min.js"
-]
+	"common/jslib/tinymce-4.0.12/ui-tinymce.js"
+];
 
-gulp.task('default', function() {
-	return gulp
-	.src(jslibs)
-    .pipe(concat('libs.js'))
-    .pipe(gulp.dest('dist/js'));
+
+gulp.task('scripts-browserify', function() {
+    gulp
+        .src('app/libs.js')
+        .pipe(browserify({insertGlobals: false, debug: false}))
+        .pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('scripts', function() {
+    return gulp
+        .src(staticLibs)
+        .pipe(concat('static-libs.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+//gulp.task('styles', function() {
+//    return gulp.src()
+//});
+
+gulp.task('default', ["scripts-browserify", "scripts"]);
