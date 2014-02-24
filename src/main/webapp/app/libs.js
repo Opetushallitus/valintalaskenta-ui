7b9bb05d4abd8d32169d965bbeb69e87d98fd510
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-02-24 19:35:49 +0200

    fronttibuildista pois jäänyt libs.js

---
+++ src/main/webapp/app/libs.js
@@ -0,0 +1,8 @@
+require('angular');
+require('angular-animate');
+require('angular-router');
+require('angular-resource');
+require('jquery');
+require('jqueryui-browser');
+require('underscore');
+
