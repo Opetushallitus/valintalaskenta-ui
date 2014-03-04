b8bd08470b1f6638a14df1d7eb8c9359269ae1e1
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-03-04 15:52:40 +0200

    fronttibuildissa browserify vaihdettu boweriin

--- src/main/webapp/app/libs.js
+++
@@ -1,8 +0,0 @@
-require('angular');
-require('angular-animate');
-require('angular-router');
-require('angular-resource');
-require('jquery');
-require('jqueryui-browser');
-require('underscore');
-
