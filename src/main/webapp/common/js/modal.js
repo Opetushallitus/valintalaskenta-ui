96ddcbe807c0c21f49a26e90c5e547df00c6d9e7
jukais <jukais@gofore.com>
2014-04-11 09:35:29 +0300

    modal ikkunan korjaus

--- src/main/webapp/common/js/modal.js
+++ src/main/webapp/common/js/modal.js
@@ -3,36 +3,32 @@
 app.directive('modal', function ($modal) {
 
     return {
+        scope: true,
         restrict: 'E',
         transclude: true,
         replace: false,
         template: '<div ng-transclude></div>',
-        link: function ($scope, $elems, $attrs) {
-            var template = $attrs.modalTemplate;
-            var windowClass = $attrs.windowClass;
-
-            $scope.show = function()
-            {
+        controller: function ($scope) {
+            $scope.show = function () {
                 $modal.open({
                     scope: $scope,
-                    templateUrl: template,
-                    windowClass: windowClass,
+                    templateUrl: $scope.modalTemplate,
+                    windowClass: $scope.windowClass,
                     controller: function ($scope, $modalInstance) {
                         $scope.sulje = function () {
                             $modalInstance.dismiss('cancel');
                         };
-
-                    },
-                    resolve: {
-
                     }
-                }).result.then(function () {
-
-                    }, function () {
-
-                    }
-                );
+                });
             };
+
+            this.show = function () {
+                $scope.show();
+            }
+        },
+        link: function ($scope, $elems, $attrs) {
+            $scope.modalTemplate = $attrs.modalTemplate;
+            $scope.windowClass = $attrs.windowClass;
         }
     };
 });
@@ -43,6 +39,11 @@ app.directive('modalOpen', function () {
         restrict: 'E',
         transclude: true, // It transcludes the contents of the directive into the template
         replace: true, // The element containing the directive will be replaced with the template
+        link: function (scope, element, attrs, modalCtrl) {
+            scope.show = function () {
+                modalCtrl.show();
+            }
+        },
         template: '<a href="" ng-click="show()" ng-transclude></a>'
     };
 });
