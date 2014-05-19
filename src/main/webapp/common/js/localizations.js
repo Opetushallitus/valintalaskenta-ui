2b3e387c09bf2089aa622d3c13e3a8df0e657838
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-05-19 15:17:59 +0300

    VT-804: varsinaisen taskin lisäksi tässä lisätään käyttöliittymäkerrokseen angular-translate -moduuli

---
+++ src/main/webapp/common/js/localizations.js
@@ -0,0 +1,63 @@
+
+
+
+
+app.config(function ($translateProvider) {
+
+    $translateProvider.translations('fi', {
+        sijoitteluntulos: {
+            header: {
+                enrollmentinfo: 'Ilmoittautumistieto'
+            },
+            enrollmentinfo: {
+                notdone: "Ei tehty",
+                present: "Läsnä (koko lukuvuosi)",
+                notpresent: "Poissa",
+                noenrollment: "Ei ilmoittautunut",
+                presentfall: "Läsnä syksy, poissa kevät",
+                notpresentfall: "Poissa syksy, läsnä kevät",
+                presentspring: "Läsnä, keväällä alkava koulutus",
+                notpresentspring: "Poissa, keväällä alkava koulutus"
+            }
+        },
+
+        discretionary: {
+            modal: {
+                title: "Harkinnanvarainen",
+                oid: "Hakemuksen tunniste",
+                state: "Harkinnanvarainen tila"
+            }
+        }
+    });
+
+
+
+    $translateProvider.translations('en', {
+        sijoitteluntulos: {
+            header: {
+                enrollmentinfo: 'Ilmoittautumistieto'
+            },
+            enrollmentinfo: {
+                notdone: "Ei tehty",
+                present: "Läsnä (koko lukuvuosi)",
+                notpresent: "Poissa",
+                noenrollment: "Ei ilmoittautunut",
+                presentfall: "Läsnä syksy, poissa kevät",
+                notpresentfall: "Poissa syksy, läsnä kevät",
+                presentspring: "Läsnä, keväällä alkava koulutus",
+                notpresentspring: "Poissa, keväällä alkava koulutus"
+            }
+        },
+
+        discretionary: {
+            modal: {
+                title: "Harkinnanvarainen",
+                oid: "Hakemuksen tunniste",
+                state: "Harkinnanvarainen tila"
+            }
+        }
+    });
+
+    $translateProvider.preferredLanguage('fi');
+});
+
