7cb7526446c0b1f7638b93a129a3e43154cca284
kallem <kalle.manninen@gofore.com>
2014-06-23 16:15:29 +0300

    VT-909 AngularJS Unit ja E2E testien luominen

--- src/test/ui/unit/controllersSpec.js
+++ src/test/ui/unit/controllersSpec.js
@@ -2,78 +2,58 @@
 
 describe('Testing HakukohteetController', function(){
     var $rootScope, $controller, $httpBackend, $location,
-        hakukohteetModel,globalStates,hakuModel,scope;
+        hakukohteetModel,globalStates,hakuModel,scope,ctrl,hakukohteetjson;
 
-    beforeEach(module('valintalaskenta'));
+    beforeEach(module('valintalaskenta','testData'));
 
-    beforeEach(inject(function($injector) {
+    beforeEach(inject(function($injector, hakukohteetJSON) {
         $httpBackend = $injector.get('$httpBackend');
         $rootScope = $injector.get('$rootScope');
 
         $controller = $injector.get('$controller');
-
-        scope = $rootScope.$new();
-
+        hakukohteetjson = hakukohteetJSON.query.results;
         hakukohteetModel = $injector.get('HakukohteetModel');
-
+        var casString = ["APP_VALINTOJENTOTEUTTAMINEN_CRUD_1.2.246.562.10.00000000001"];
+        $httpBackend.expectGET('/cas/myroles').respond(casString);
+        $httpBackend.expectGET('buildversion.txt?auth').respond("1.0");
     }));
 
-    it('should have', function() {
-
+    it('should get hakukohteet', function() {
+        scope = $rootScope.$new();
         var routeParams = {"hakukohdeOid": "oid2"};
         globalStates = {
             hakukohteetVisible : true
         };
 
-        var hakukohdeString = {
-            "kokonaismaara" : 2,
-            "tulokset" : [ {
-                "hakukohdeOid" : "1.2.246.562.20.59262166669",
-                "tarjoajaOid" : "1.2.246.562.10.74903763345",
-                "tarjoajaNimi" : {
-                    "fi" : "Kauppiaitten Kauppaoppilaitos"
-                },
-                "hakukohdeNimi" : {
-                    "fi" : "Liiketalouden perustutkinto, yo",
-                    "sv" : "Grundexamen inom företagsekonomi, st",
-                    "en" : "Liiketalouden perustutkinto, yo"
-                },
-                "hakukohdeTila" : "JULKAISTU",
-                "hakuVuosi" : 0,
-                "koulutusVuosi" : 0
-            }, {
-                "hakukohdeOid" : "1.2.246.562.20.799065290810",
-                "tarjoajaOid" : "1.2.246.562.10.40503922409",
-                "tarjoajaNimi" : {
-                    "fi" : "Tampereen seudun ammattiopisto Tredu, Oriveden toimipiste"
-                },
-                "hakukohdeNimi" : {
-                    "fi" : "Kalatalouden perustutkinto, yo",
-                    "sv" : "Grundexamen i fiskeri, st",
-                    "en" : "Kalatalouden perustutkinto, yo"
-                },
-                "hakukohdeTila" : "JULKAISTU",
-                "hakuVuosi" : 0,
-                "koulutusVuosi" : 0
-            }]
-        };
-        var casString = ["APP_VALINTOJENTOTEUTTAMINEN_CRUD_1.2.246.562.10.00000000001"];
-
-        $httpBackend.expectGET('/cas/myroles').respond(casString);
-        $httpBackend.expectGET('buildversion.txt?auth').respond("1.0");
-        waits(500);
-        $httpBackend.expectGET('haku/hakukohdeTulos?count=15&hakukohdeTilas=JULKAISTU&organisationOids=1.2.246.562.10.00000000001&searchTerms=&startIndex=0').respond(hakukohdeString);
-
-
-        var ctrl = $controller(HakukohteetController, {'$rootScope' : $rootScope, '$scope' : scope,
+        ctrl = $controller(HakukohteetController, {'$rootScope' : $rootScope, '$scope' : scope,
             '$location': $location, '$routeParams': routeParams, 'HakukohteetModel': hakukohteetModel,
             'GlobalStates': globalStates, 'HakuModel': hakuModel});
+
+
         $rootScope.$apply();
+        $httpBackend.flush();
+        waits(500);
+        $httpBackend.expectGET('haku/hakukohdeTulos?count=15&hakukohdeTilas=JULKAISTU&organisationOids=1.2.246.562.10.00000000001&searchTerms=&startIndex=0')
+            .respond(201,hakukohteetjson);
+    });
+
+    it('check initialized variables', function() {
+        expect(scope.model.hakukohteet.length).toBe(16);
+        expect(scope.model.filtered.length).toBe(0);
+        expect(scope.model.totalCount).toBe(100);
+        expect(scope.model.pageSize).toBe(15);
+        expect(scope.model.searchWord).toBe("");
+        expect(scope.model.lastSearch).toBe("");
+        expect(scope.model.lastHakuOid).toBe(null);
+        expect(scope.model.omatHakukohteet).toBe(true);
+        expect(scope.model.valmiitHakukohteet).toBe("JULKAISTU");
+        expect(scope.model.readyToQueryForNextPage).toBe(true);
+
     });
 
 
-    afterEach(function() {
 
+    afterEach(function() {
         $rootScope.$apply();
         $httpBackend.flush();
         $httpBackend.verifyNoOutstandingExpectation();
---
+++ src/test/ui/unit/testdata.js
@@ -0,0 +1,236 @@
+'use strict';
+
+angular.module('testData', [])
+    .value('hakukohteetJSON', {
+        query: {
+            count: 1,
+            results:  {
+            "kokonaismaara" : 100,
+            "tulokset" : [ {
+                "hakukohdeOid" : "1.2.246.562.5.39836447563",
+                "tarjoajaOid" : "1.2.246.562.10.60222091211",
+                "tarjoajaNimi" : {
+                    "fi" : "Ahlmanin ammattiopisto"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Puutarhatalouden perustutkinto, yo",
+                    "sv" : "Grundexamen i trädgårdsskötsel, st",
+                    "en" : "Puutarhatalouden perustutkinto, yo"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.346485857010",
+                "tarjoajaOid" : "1.2.246.562.10.60222091211",
+                "tarjoajaNimi" : {
+                    "fi" : "Ahlmanin ammattiopisto"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Viheralan koulutusohjelma, yo (Puutarhatalouden perustutkinto)",
+                    "sv" : "Utbildningsprogrammet för grönsektorn, st (Grundexamen i trädgårdsskötsel)",
+                    "en" : "Viheralan koulutusohjelma, yo (Puutarhatalouden perustutkinto)"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.20.85144636629",
+                "tarjoajaOid" : "1.2.246.562.10.60222091211",
+                "tarjoajaNimi" : {
+                    "fi" : "Ahlmanin ammattiopisto"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Kaivosalan perustutkinto, yo",
+                    "sv" : "Grundexamen inom gruvbranschen, st",
+                    "en" : "Kaivosalan perustutkinto, yo"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.461921204610",
+                "tarjoajaOid" : "1.2.246.562.10.60222091211",
+                "tarjoajaNimi" : {
+                    "fi" : "Ahlmanin ammattiopisto"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Hotelli-, ravintola- ja catering-alan perustutkinto, pk",
+                    "sv" : "Grundexamen inom hotell-, restaurang- och cateringbranschen, gr",
+                    "en" : "Hotelli-, ravintola- ja catering-alan perustutkinto, pk"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.28277163201",
+                "tarjoajaOid" : "1.2.246.562.10.60222091211",
+                "tarjoajaNimi" : {
+                    "fi" : "Ahlmanin ammattiopisto"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Matkailualan perustutkinto, yo",
+                    "sv" : "Grundexamen i turism, st",
+                    "en" : "Matkailualan perustutkinto, yo"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.85532589612",
+                "tarjoajaOid" : "1.2.246.562.10.60222091211",
+                "tarjoajaNimi" : {
+                    "fi" : "Ahlmanin ammattiopisto"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Eläintenhoidon koulutusohjelma, pk (Maatalousalan perustutkinto)",
+                    "sv" : "Utbildningsprogrammet för djurskötsel, gr (Grundexamen inom lantbruksbranschen)",
+                    "en" : "Eläintenhoidon koulutusohjelma, pk (Maatalousalan perustutkinto)"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.72457229796",
+                "tarjoajaOid" : "1.2.246.562.10.39550106221",
+                "tarjoajaNimi" : {
+                    "fi" : "Akaan lukio"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Lukio",
+                    "sv" : "Gymnasium",
+                    "en" : "Lukio"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.52911964691",
+                "tarjoajaOid" : "1.2.246.562.10.50460470417",
+                "tarjoajaNimi" : {
+                    "fi" : "Alajärven lukio"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Lukio",
+                    "sv" : "Gymnasium",
+                    "en" : "Lukio"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.24389514902",
+                "tarjoajaOid" : "1.2.246.562.10.10464399921",
+                "tarjoajaNimi" : {
+                    "fi" : "Alavuden lukio"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Lukio",
+                    "sv" : "Gymnasium",
+                    "en" : "Lukio"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.88341152242",
+                "tarjoajaOid" : "1.2.246.562.10.71323338948",
+                "tarjoajaNimi" : {
+                    "fi" : "Alppilan lukio"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Lukio",
+                    "sv" : "Gymnasium",
+                    "en" : "Lukio"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.65137341411",
+                "tarjoajaOid" : "1.2.246.562.10.72066389551",
+                "tarjoajaNimi" : {
+                    "fi" : "Ammatti-instituutti Iisakki"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Sähkö- ja automaatiotekniikan perustutkinto, pk",
+                    "sv" : "Grundexamen inom el- och automationsteknik, gr",
+                    "en" : "Sähkö- ja automaatiotekniikan perustutkinto, pk"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.814339042410",
+                "tarjoajaOid" : "1.2.246.562.10.72066389551",
+                "tarjoajaNimi" : {
+                    "fi" : "Ammatti-instituutti Iisakki"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Rakennusalan perustutkinto, pk",
+                    "sv" : "Grundexamen inom byggnadsbranschen, gr",
+                    "en" : "Rakennusalan perustutkinto, pk"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.88559320218",
+                "tarjoajaOid" : "1.2.246.562.10.72066389551",
+                "tarjoajaNimi" : {
+                    "fi" : "Ammatti-instituutti Iisakki"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Hotelli-, ravintola- ja catering-alan perustutkinto, pk",
+                    "sv" : "Grundexamen inom hotell-, restaurang- och cateringbranschen, gr",
+                    "en" : "Hotelli-, ravintola- ja catering-alan perustutkinto, pk"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.95759319643",
+                "tarjoajaOid" : "1.2.246.562.10.72066389551",
+                "tarjoajaNimi" : {
+                    "fi" : "Ammatti-instituutti Iisakki"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Sähkö- ja automaatiotekniikan perustutkinto, yo",
+                    "sv" : "Grundexamen inom el- och automationsteknik, st",
+                    "en" : "Sähkö- ja automaatiotekniikan perustutkinto, yo"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.80346194402",
+                "tarjoajaOid" : "1.2.246.562.10.72066389551",
+                "tarjoajaNimi" : {
+                    "fi" : "Ammatti-instituutti Iisakki"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Prosessiteollisuuden perustutkinto, pk",
+                    "sv" : "Grundexamen inom processindustrin, gr",
+                    "en" : "Prosessiteollisuuden perustutkinto, pk"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }, {
+                "hakukohdeOid" : "1.2.246.562.5.34684990186",
+                "tarjoajaOid" : "1.2.246.562.10.72066389551",
+                "tarjoajaNimi" : {
+                    "fi" : "Ammatti-instituutti Iisakki"
+                },
+                "hakukohdeNimi" : {
+                    "fi" : "Kone- ja metallialan perustutkinto, pk",
+                    "sv" : "Grundexamen inom maskin- och metallbranschen, gr",
+                    "en" : "Kone- ja metallialan perustutkinto, pk"
+                },
+                "hakukohdeTila" : "JULKAISTU",
+                "hakuVuosi" : 0,
+                "koulutusVuosi" : 0
+            }]
+        }
+    }
+});
