b0fe4f083e0ac5a7ba5346787783bbdce1bae369
kallem <kalle.manninen@gofore.com>
2014-06-24 15:16:39 +0300

    VT-909 AngularJS Unit ja E2E testien luominen

--- src/test/ui/unit/controllersSpec.js
+++ src/test/ui/unit/controllersSpec.js
@@ -11,18 +11,17 @@ describe('Testing HakukohteetController', function(){
         $rootScope = $injector.get('$rootScope');
         $location = $injector.get('$location');
         $controller = $injector.get('$controller');
-        hakukohteetjson = hakukohteetJSON.query.results;
+        hakukohteetjson = hakukohteetJSON;
         hakukohteetModel = $injector.get('HakukohteetModel');
         var casString = ["APP_VALINTOJENTOTEUTTAMINEN_CRUD_1.2.246.562.10.00000000001"];
         $httpBackend.expectGET('/cas/myroles').respond(casString);
         $httpBackend.expectGET('buildversion.txt?auth').respond("1.0");
     }));
 
-    it('should get hakukohteet', function(GlobalStates) {
+    it('should get hakukohteet', function() {
         scope = $rootScope.$new();
         rootScope = $rootScope;
         location = $location;
-        globalStates = GlobalStates;
         globalStates = {
             hakukohteetVisible : true
         };
@@ -99,4 +98,140 @@ describe('Testing HakukohteetController', function(){
     });
 
 
+});
+
+describe('Testing HenkiloController', function(){
+    var rootScope,$rootScope, $controller, $httpBackend, $location, location,
+        henkiloModel,scope,ctrl,hakemuksetjson;
+    var routeParams = {"hakuOid": "oid"};
+    beforeEach(module('valintalaskenta','testData'));
+
+    beforeEach(inject(function($injector, hakemuksetJSON) {
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        $location = $injector.get('$location');
+        $controller = $injector.get('$controller');
+        hakemuksetjson = hakemuksetJSON;
+        henkiloModel = $injector.get('HenkiloModel');
+        var casString = ["APP_VALINTOJENTOTEUTTAMINEN_CRUD_1.2.246.562.10.00000000001"];
+        $httpBackend.expectGET('/cas/myroles').respond(casString);
+        $httpBackend.expectGET('buildversion.txt?auth').respond("1.0");
+    }));
+
+    it('should get henkilöt', function() {
+        scope = $rootScope.$new();
+        rootScope = $rootScope;
+        location = $location;
+
+        $httpBackend.expectGET('haku-app/applications?appState=ACTIVE&appState=INCOMPLETE&q=&rows=30&start=0')
+            .respond(201,hakemuksetjson);
+
+        ctrl = $controller(HenkiloController, {'$scope' : scope,
+            '$location': location, '$routeParams': routeParams, 'HenkiloModel': henkiloModel});
+    });
+
+    it('check initialized variables', function() {
+        expect(scope.model.hakemukset.length).toBe(60);
+        expect(scope.model.totalCount).toBe(5012);
+        expect(scope.model.pageSize).toBe(30);
+        expect(scope.model.searchWord).toBe("");
+        expect(scope.model.lastSearch).toBe("");
+        expect(scope.model.readyToQueryForNextPage).toBe(true);
+    });
+
+    it('toggleHenkiloittainVisible', function() {
+        scope.toggleHenkiloittainVisible();
+        expect(scope.henkiloittainVisible).toBe(false);
+        scope.toggleHenkiloittainVisible();
+        expect(scope.henkiloittainVisible).toBe(true);
+    });
+
+    it('showHakemus', function() {
+        var hakemus = {
+            oid: '1.2.246.562.5.39836447563'
+        };
+
+        scope.showHakemus(hakemus);
+        expect(location.path()).toMatch('/haku/' + routeParams.hakuOid + '/henkiloittain/' + hakemus.oid +
+            "/henkilotiedot/id_" + hakemus.oid);
+
+    });
+
+    afterEach(function() {
+        $rootScope.$apply();
+        $httpBackend.flush();
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+});
+
+describe('Testing SijoitteluntulosController', function(){
+    var rootScope,$rootScope, $controller, $httpBackend, $location, location,
+        scope,ctrl,hakukohdenimijson,hakukohdetilajson,hakujson,hakukohdejson, sijoitteluajojson, $modal, $window,kirjepohjat,latausikkuna,hakukohdeModel,
+        sijoitteluntulosModel, osoitetarratSijoittelussaHyvaksytyille, hyvaksymiskirjeet, jalkiohjauskirjeet,
+        sijoitteluXls, authService;
+    var routeParams = {"hakuOid": "oid",
+        "hakukohdeOid" : "oid2"};
+    beforeEach(module('valintalaskenta','testData'));
+
+    beforeEach(inject(function($injector, hakuJSON, hakukohdeJSON, sijoitteluajoJSON, hakukohdenimiJSON,
+                               hakukohdetilaJSON) {
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        $location = $injector.get('$location');
+        $controller = $injector.get('$controller');
+        hakujson = hakuJSON;
+        hakukohdejson = hakukohdeJSON;
+        sijoitteluajojson = sijoitteluajoJSON;
+        hakukohdenimijson = hakukohdenimiJSON;
+        hakukohdetilajson = hakukohdetilaJSON;
+        hakukohdeModel = $injector.get('HakukohdeModel');
+        $modal = $injector.get('$modal');
+        $window = $injector.get('$window');
+        kirjepohjat = $injector.get('Kirjepohjat');
+        latausikkuna = $injector.get('Latausikkuna');
+        sijoitteluntulosModel = $injector.get('SijoitteluntulosModel');
+        osoitetarratSijoittelussaHyvaksytyille = $injector.get('OsoitetarratSijoittelussaHyvaksytyille');
+        hyvaksymiskirjeet = $injector.get('Hyvaksymiskirjeet');
+        jalkiohjauskirjeet = $injector.get('Jalkiohjauskirjeet');
+        sijoitteluXls = $injector.get('SijoitteluXls');
+        authService = $injector.get('AuthService');
+
+        var casString = ["APP_VALINTOJENTOTEUTTAMINEN_CRUD_1.2.246.562.10.00000000001"];
+        $httpBackend.expectGET('/cas/myroles').respond(casString);
+        $httpBackend.expectGET('buildversion.txt?auth').respond("1.0");
+    }));
+
+    it('should get sijoitteluntulokset', function() {
+        scope = $rootScope.$new();
+        rootScope = $rootScope;
+        location = $location;
+
+        $httpBackend.expectGET('hakukohde/'+routeParams.hakukohdeOid)
+            .respond(201,hakukohdejson);
+        $httpBackend.expectGET('haku/'+routeParams.hakuOid)
+            .respond(201,hakujson);
+        $httpBackend.expectGET('resources/sijoittelu/'+routeParams.hakuOid+'/sijoitteluajo/latest/hakukohde/'+routeParams.hakukohdeOid)
+            .respond(201,sijoitteluajojson);
+        $httpBackend.expectGET('organisaatio/undefined/parentoids')
+            .respond(201,'1.2.246.562.10.00000000001/undefined');
+        $httpBackend.expectGET('hakukohde/'+routeParams.hakukohdeOid+'/nimi')
+            .respond(201,hakukohdenimijson);
+        $httpBackend.expectGET('organisaatio/1.2.246.562.10.60222091211/parentoids')
+            .respond(201,'1.2.246.562.10.00000000001/1.2.246.562.10.47941294986/1.2.246.562.10.98873174761/1.2.246.562.10.60222091211');
+        $httpBackend.expectGET('resources/tila/hakukohde/'+routeParams.hakukohdeOid+'/1397647295344-8565235898154713515')
+            .respond(201,hakukohdetilajson);
+        ctrl = $controller(SijoitteluntulosController, {'$scope' : scope, '$modal' : $modal, '$routeParams': routeParams,
+            '$window' : $window, 'Kirjepohjat': kirjepohjat, 'Latausikkuna': latausikkuna, 'HakukohdeModel': hakukohdeModel,
+            'SijoitteluntulosModel': sijoitteluntulosModel, 'OsoitetarratSijoittelussaHyvaksytyille': osoitetarratSijoittelussaHyvaksytyille,
+            'Hyvaksymiskirjeet': hyvaksymiskirjeet, 'Jalkiohjauskirjeet': jalkiohjauskirjeet, 'SijoitteluXls': sijoitteluXls,
+            'AuthService': authService});
+    });
+
+    afterEach(function() {
+        $rootScope.$apply();
+        $httpBackend.flush();
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
 });
