af454fa59b3356eda5ffb2e9ed8d34b69ca862f0
kallem <kalle.manninen@gofore.com>
2014-08-13 13:08:04 +0300

    VT-907 Valintalaskennan käynnistäminen valintojen henkilönäkymässä

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -384,6 +384,13 @@ app.factory('Dokumenttipalvelu', function($http, $log, $rootScope, $resource, $w
 });
 
 //koostepalvelus
+app.factory('ValintalaskentaKerrallaHakukohteille', function($resource) {
+    return $resource(VALINTALASKENTAKOOSTE_URL_BASE + "resources/valintalaskentakerralla/haku/:hakuoid/whitelist/:whitelist",
+        {hakuoid: "@hakuoid", whitelist: "@whitelist"}, {
+        aktivoi: {method: "POST"}
+    });
+});
+
 app.factory('ValintakoelaskentaAktivointi', function($resource) {
     return $resource(VALINTALASKENTAKOOSTE_URL_BASE + "resources/valintakoelaskenta/aktivoiValintakoelaskenta", {}, {
         aktivoi: {method: "POST"}
