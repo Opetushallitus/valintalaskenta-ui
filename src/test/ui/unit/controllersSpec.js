948966a0b0d8cd84c86458556422abb4e4a30ce0
Kalle Manninen <kalle.manninen@gofore.com>
2014-09-16 17:35:20 +0300

    VT-949 Ajastetun sijoittelun tiheyden määritteleminen käyttöliittymässä

--- src/test/ui/unit/controllersSpec.js
+++ src/test/ui/unit/controllersSpec.js
@@ -293,12 +293,12 @@ describe('Testing SijoitteluntulosController', function(){
 
     it('isKorkeakoulu', function() {
         scope.model.haku.kohdejoukkoUri = "uri_1";
-        expect(scope.hakemuksenMuokattuVastaanottoTilat.length).toBe(5);
+        expect(scope.hakemuksenMuokattuVastaanottoTilat.length).toBe(4);
         scope.isKorkeakoulu();
-        expect(scope.hakemuksenMuokattuVastaanottoTilat.length).toBe(5);
+        expect(scope.hakemuksenMuokattuVastaanottoTilat.length).toBe(4);
         scope.model.haku.kohdejoukkoUri = "uri_12";
         scope.isKorkeakoulu();
-        expect(scope.hakemuksenMuokattuVastaanottoTilat.length).toBe(6);
+        expect(scope.hakemuksenMuokattuVastaanottoTilat.length).toBe(5);
     });
 
     it('resetIlmoittautumisTila', function() {
