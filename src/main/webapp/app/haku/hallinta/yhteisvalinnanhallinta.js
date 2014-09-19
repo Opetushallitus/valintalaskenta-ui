ad572dec043d6f6ac3cd9045fdcd65c86a74eb28
Kalle Manninen <kalle.manninen@gofore.com>
2014-09-19 08:24:34 +0300

    VT-949 Ajastetun sijoittelun tiheyden määritteleminen käyttöliittymässä

--- src/main/webapp/app/haku/hallinta/yhteisvalinnanhallinta.js
+++ src/main/webapp/app/haku/hallinta/yhteisvalinnanhallinta.js
@@ -339,15 +339,17 @@ angular.module('valintalaskenta').
     };
 
     $scope.paivitaJatkuvanSijoittelunAloitus = function () {
-        $scope.jatkuva.aloitusajankohta = new Date($scope.jatkuva.aloitusajankohta_date);
-        $scope.jatkuva.aloitusajankohta.setHours(new Date($scope.jatkuva.aloitusajankohta_time).getHours(),new Date($scope.jatkuva.aloitusajankohta_time).getMinutes());
+        if ($scope.jatkuva.ajotiheys && $scope.jatkuva.aloitusajankohta_date && $scope.jatkuva.aloitusajankohta_time) {
+            $scope.jatkuva.aloitusajankohta = new Date($scope.jatkuva.aloitusajankohta_date);
+            $scope.jatkuva.aloitusajankohta.setHours(new Date($scope.jatkuva.aloitusajankohta_time).getHours(), new Date($scope.jatkuva.aloitusajankohta_time).getMinutes());
 
-        JatkuvaSijoittelu.get({hakuOid: $routeParams.hakuOid, aloitusajankohta: new Date($scope.jatkuva.aloitusajankohta).getTime(), ajotiheys: $scope.jatkuva.ajotiheys,
-            method: 'paivita'}, function (result) {
-            $route.reload();
-        }, function (error) {
-            alert("virhe");
-        });
+            JatkuvaSijoittelu.get({hakuOid: $routeParams.hakuOid, aloitusajankohta: new Date($scope.jatkuva.aloitusajankohta).getTime(), ajotiheys: $scope.jatkuva.ajotiheys,
+                method: 'paivita'}, function (result) {
+                $route.reload();
+            }, function (error) {
+                alert("virhe");
+            });
+        }
     };
 
     JatkuvaSijoittelu.get({hakuOid: $routeParams.hakuOid}, function (result) {
