4bf62ae0108034d882bb58b85a73b9c4991de906
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-10-29 15:16:02 +0200

    VT-1045: kopioitu valintalaskentatuloksen näkymän toteutus erillishakuun

--- src/main/webapp/app/haku/hakukohteet/erillishaku/erillishaku.js
+++ src/main/webapp/app/haku/hakukohteet/erillishaku/erillishaku.js
@@ -1,27 +1,215 @@
-﻿app.factory('ErllishakuModel', function($routeParams, ValinnanvaiheListByHakukohde, JarjestyskriteeriMuokattuJonosija,
+﻿angular.module('valintalaskenta')
+
+.factory('ErillishakuModel', ['$routeParams', 'ValinnanvaiheListByHakukohde', 'JarjestyskriteeriMuokattuJonosija',
+        'ValinnanVaiheetIlmanLaskentaa', 'HakukohdeHenkilotFull', 'Ilmoitus', 'IlmoitusTila', '$q', 'ValintaperusteetHakukohde', 'ValintatapajonoSijoitteluStatus',
+        function($routeParams, ValinnanvaiheListByHakukohde, JarjestyskriteeriMuokattuJonosija,
     ValinnanVaiheetIlmanLaskentaa, HakukohdeHenkilotFull, Ilmoitus, IlmoitusTila, $q, ValintaperusteetHakukohde, ValintatapajonoSijoitteluStatus) {
     "use strict";
 
     var model;
 	model = new function() {
-	}();
+
+        this.hakukohdeOid = {};
+        this.valinnanvaiheet = [];
+        this.ilmanlaskentaa = [];
+        this.errors = [];
+        this.hakukohdeOid = "";
+        this.tarjoajaOid = "";
+        this.hakeneet = [];
+
+
+        this.refresh = function(hakukohdeOid, hakuOid) {
+            var defer = $q.defer();
+
+            model.hakukohdeOid = {};
+            model.valinnanvaiheet = [];
+            model.ilmanlaskentaa = [];
+            model.errors = [];
+            model.errors.length = 0;
+            model.hakukohdeOid = hakukohdeOid;
+            model.tarjoajaOid = "";
+            model.hakeneet = [];
+            
+            ValintaperusteetHakukohde.get({hakukohdeoid: hakukohdeOid}, function(result) {
+                model.tarjoajaOid = result.tarjoajaOid;
+            });
+            ValinnanvaiheListByHakukohde.get({hakukohdeoid: hakukohdeOid}, function(result) {
+                model.valinnanvaiheet = result;
+
+                ValinnanVaiheetIlmanLaskentaa.get({hakukohdeoid: hakukohdeOid}, function(result) {
+                    model.ilmanlaskentaa = result;
+
+                    if(result.length > 0) {
+                        HakukohdeHenkilotFull.get({aoOid: hakukohdeOid, rows: 100000}, function (result) {
+                            model.hakeneet = result;
+                            model.ilmanlaskentaa.forEach(function (vaihe) {
+                                vaihe.valintatapajonot = [];
+                                vaihe.hakuOid = hakuOid;
+                                vaihe.jonot.forEach(function(jono) {
+                                    var tulosjono = {};
+                                    tulosjono.oid = jono.oid;
+                                    tulosjono.valintatapajonooid = jono.oid;
+                                    tulosjono.prioriteetti = jono.prioriteetti;
+                                    tulosjono.aloituspaikat = jono.aloituspaikat;
+
+                                    if(jono.siirretaanSijoitteluun == null) {
+                                        tulosjono.siirretaanSijoitteluun = true;
+                                    } else {
+                                        tulosjono.siirretaanSijoitteluun = jono.siirretaanSijoitteluun;
+                                    }
+                                    if(jono.valmisSijoiteltavaksi == null) {
+                                        tulosjono.siirretaanSijoitteluun = true;
+                                    } else {
+                                        tulosjono.siirretaanSijoitteluun = jono.valmisSijoiteltavaksi;
+                                    }
+                                    if(jono.tasapistesaanto == null) {
+                                        tulosjono.tasasijasaanto = 'ARVONTA'
+                                    } else {
+                                        tulosjono.tasasijasaanto = jono.tasapistesaanto;
+                                    }
+                                    if(jono.eiVarasijatayttoa == null) {
+                                        tulosjono.eiVarasijatayttoa = false;
+                                    } else {
+                                        tulosjono.eiVarasijatayttoa = jono.eiVarasijatayttoa;
+                                    }
+                                    if(jono.kaikkiEhdonTayttavatHyvaksytaan == null) {
+                                        tulosjono.kaikkiEhdonTayttavatHyvaksytaan = false;
+                                    } else {
+                                        tulosjono.kaikkiEhdonTayttavatHyvaksytaan = jono.kaikkiEhdonTayttavatHyvaksytaan;
+                                    }
+                                    if(jono.poissaOlevaTaytto == null) {
+                                        tulosjono.poissaOlevaTaytto = false;
+                                    } else {
+                                        tulosjono.poissaOlevaTaytto = jono.poissaOlevaTaytto;
+                                    }
+                                    if(jono.kaytetaanValintalaskentaa == null) {
+                                        tulosjono.kaytetaanValintalaskentaa = true;
+                                    } else {
+                                        tulosjono.kaytetaanValintalaskentaa = jono.kaytetaanValintalaskentaa;
+                                    }
+
+                                    tulosjono.nimi = jono.nimi;
+                                    tulosjono.jonosijat = [];
+
+                                    var jonosijat = _.chain(model.valinnanvaiheet)
+                                        .filter(function(current) {return current.valinnanvaiheoid == vaihe.oid})
+                                        .map(function(current) {return current.valintatapajonot})
+                                        .first()
+                                        .filter(function(tulosjono) {return tulosjono.oid == jono.oid})
+                                        .map(function(tulosjono) {return tulosjono.jonosijat})
+                                        .first()
+                                        .value();
+
+                                    model.hakeneet.forEach(function(hakija) {
+                                        var jonosija = _.findWhere(jonosijat, {hakemusOid : hakija.oid});
+                                        if(jonosija) {
+                                            var krit = jonosija.jarjestyskriteerit[0];
+                                            if(krit.tila != 'HYVAKSYTTAVISSA') {
+                                                delete jonosija.jonosija
+                                            } else {
+                                                jonosija.jonosija = -(krit.arvo);
+                                            }
+                                            tulosjono.jonosijat.push(jonosija);
+                                        } else {
+                                            jonosija = {};
+                                            jonosija.hakemusOid = hakija.oid;
+                                            jonosija.hakijaOid = null;
+                                            jonosija.prioriteetti = model.hakutoivePrioriteetti(hakija.oid);
+                                            jonosija.harkinnanvarainen = false;
+                                            jonosija.historiat = null;
+                                            jonosija.syotetytArvot = [];
+                                            jonosija.funktioTulokset = [];
+                                            jonosija.muokattu = false;
+                                            jonosija.sukunimi = hakija.answers.henkilotiedot.Sukunimi;
+                                            jonosija.etunimi = hakija.answers.henkilotiedot.Etunimet;
+                                            jonosija.jarjestyskriteerit = [
+                                                {
+                                                    arvo: null,
+                                                    tila: "",
+                                                    kuvaus: { },
+                                                    prioriteetti: 0,
+                                                    nimi: ""
+                                                }
+                                            ];
+                                            tulosjono.jonosijat.push(jonosija);
+                                        }
+
+                                    });
+                                    vaihe.valintatapajonot.push(tulosjono);
+
+                                });
+                            });
+                            model.valinnanvaiheet.forEach(function(vaihe, index) {
+                                vaihe.valintatapajonot.forEach(function(jono, i) {
+                                    if(jono.kaytetaanValintalaskentaa == false) {
+                                        vaihe.valintatapajonot.splice(i, 1);
+                                    }
+                                });
+                                if(vaihe.valintatapajonot.length <= 0) {
+                                    model.valinnanvaiheet.splice(index, 1);
+                                }
+                            });
+                            defer.resolve();
+                        }, function (error) {
+                            model.errors.push(error);
+                            defer.reject("hakukohteen tietojen hakeminen epäonnistui");
+                        });
+                    }
+                }, function(error) {
+                    model.errors.push(error);
+                    defer.reject("hakukohteen tietojen hakeminen epäonnistui");
+                });
+            }, function(error) {
+                model.errors.push(error);
+                defer.reject("hakukohteen tietojen hakeminen epäonnistui");
+            });
+
+            return defer.promise;
+        };
+
+        this.hakutoivePrioriteetti = function(hakemusoid) {
+            var hakija = _.findWhere(model.hakeneet, {oid:hakemusoid});
+            if (!hakija) {
+                return -1;
+            }
+            var toive = (_.invert(hakija.answers.hakutoiveet))[model.hakukohdeOid];
+
+            if(toive) {
+                return parseInt(toive.substring(10,11));
+            } else {
+                return -1;
+            }
+        };
+
+        this.muutaSijoittelunStatus = function(jono, status) {
+            ValintatapajonoSijoitteluStatus.put({valintatapajonoOid: jono.oid, status: status},function(result) {
+                jono.valmisSijoiteltavaksi = status;
+                Ilmoitus.avaa("Tallennus onnistui", "Tallennus onnistui.");
+            }, function(error) {
+                Ilmoitus.avaa("Tallennus epäonnistui", "Tallennus epäonnistui. Ole hyvä ja yritä hetken päästä uudelleen.", IlmoitusTila.ERROR);
+            });
+        };
+
+
+	};
 
 	return model;
-});
+}])
 
 
-angular.module('valintalaskenta').
-    controller('ErillishakuController', ['$scope', '$location', '$routeParams', '$timeout', '$upload', 'Ilmoitus',
-        'IlmoitusTila', 'Latausikkuna', 'ValintatapajonoVienti','ErllishakuModel',
+
+    .controller('ErillishakuController', ['$scope', '$location', '$routeParams', '$timeout', '$upload', 'Ilmoitus',
+        'IlmoitusTila', 'Latausikkuna', 'ValintatapajonoVienti','ErillishakuModel',
         'TulosXls', 'HakukohdeModel', '$http', 'AuthService', 'UserModel',
     function ($scope, $location, $routeParams, $timeout,  $upload, Ilmoitus, IlmoitusTila, Latausikkuna,
-              ValintatapajonoVienti,ErllishakuModel, TulosXls, HakukohdeModel, $http, AuthService, UserModel) {
+              ValintatapajonoVienti,ErillishakuModel, TulosXls, HakukohdeModel, $http, AuthService, UserModel) {
     "use strict";
 
     $scope.hakukohdeOid = $routeParams.hakukohdeOid;
     $scope.hakuOid =  $routeParams.hakuOid;
     $scope.HAKEMUS_UI_URL_BASE = HAKEMUS_UI_URL_BASE;
-    $scope.model = ErllishakuModel;
+    $scope.model = ErillishakuModel;
+        ErillishakuModel.refresh($scope.hakukohdeOid, $scope.hakuOid);
     $scope.hakukohdeModel = HakukohdeModel;
 
     var hakukohdeModelpromise = HakukohdeModel.refreshIfNeeded($routeParams.hakukohdeOid);
@@ -48,5 +236,115 @@ angular.module('valintalaskenta').
     UserModel.refreshIfNeeded().then(function(){
         $scope.jkmuokkaus = UserModel.isKKUser;
     });
+
+
+        for (var i = 0; i < 1000; i++) {
+            $scope.currentPage[i] = [];
+            $scope.filteredResults[i] = [];
+            for (var j = 0; j < 1000; j++) {
+                $scope.currentPage[i][j] = 1;
+            }
+        }
+
+        $scope.valintatapajonoVientiXlsx = function(valintatapajonoOid) {
+            ValintatapajonoVienti.vie({
+                    valintatapajonoOid: valintatapajonoOid,
+                    hakukohdeOid: $scope.hakukohdeOid,
+                    hakuOid: $routeParams.hakuOid},
+                {}, function (id) {
+                    Latausikkuna.avaa(id, "Valintatapajonon vienti taulukkolaskentaan", "");
+                }, function () {
+                    Ilmoitus.avaa("Valintatapajonon vienti epäonnistui! Ota yhteys ylläpitoon.", IlmoitusTila.ERROR);
+                });
+        };
+
+        $scope.valintatapajonoTuontiXlsx = function(valintatapajonoOid, $files) {
+            var file = $files[0];
+            var fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(file);
+            var hakukohdeOid = $scope.hakukohdeOid;
+            var hakuOid = $routeParams.hakuOid;
+            fileReader.onload = function(e) {
+                $scope.upload = $upload.http({
+                    url: VALINTALASKENTAKOOSTE_URL_BASE + "resources/valintatapajonolaskenta/tuonti?hakuOid=" +hakuOid + "&hakukohdeOid=" +hakukohdeOid + "&valintatapajonoOid="+ valintatapajonoOid, //upload.php script, node.js route, or servlet url
+                    method: "POST",
+                    headers: {'Content-Type': 'application/octet-stream'},
+                    data: e.target.result
+                }).progress(function(evt) {
+                    //console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function(id, status, headers, config) {
+                    Latausikkuna.avaaKustomoitu(id, "Valintatapajonon tuonti", "", "../common/modaalinen/tuontiikkuna.html",
+                        function(dokumenttiId) {
+                            // tee paivitys
+                            $scope.model.refresh(hakukohdeOid, hakuOid);
+                        }
+                    );
+                }).error(function(data) {
+                    //error
+                });
+            };
+        };
+
+        $scope.valintalaskentaTulosXLS = function() {
+            TulosXls.query({hakukohdeOid:$routeParams.hakukohdeOid});
+        };
+
+        $scope.showHistory = function(valintatapajonoOid, hakemusOid) {
+            $location.path('/valintatapajono/' + valintatapajonoOid + '/hakemus/' + hakemusOid + '/valintalaskentahistoria');
+        };
+
+        $scope.showTilaPartial = function(valintatulos) {
+            if(valintatulos.showTilaPartial === null || valintatulos.showTilaPartial === false) {
+                valintatulos.showTilaPartial = true;
+            } else {
+                valintatulos.showTilaPartial = false;
+            }
+        };
+        $scope.showHenkiloPartial = function(valintatulos) {
+            if(valintatulos.showHenkiloPartial === null || valintatulos.showHenkiloPartial === false) {
+                valintatulos.showHenkiloPartial = true;
+            } else {
+                valintatulos.showHenkiloPartial = false;
+            }
+        };
+
+
+
+
+
+
+
+
+
+        $scope.submit = function (vaiheoid, jonooid) {
+            ValintalaskentatulosModel.submit(vaiheoid, jonooid);
+        };
+
+        $scope.muutaSijoittelunStatus = function (jono, status) {
+            ValintalaskentatulosModel.muutaSijoittelunStatus(jono, status);
+        };
+
+        $scope.changeTila = function (jonosija, value) {
+            if (_.isNumber(value)) {
+                $timeout(function(){
+                    jonosija.tuloksenTila = "HYVAKSYTTAVISSA";
+                });
+            } else {
+                $timeout(function(){
+                    jonosija.tuloksenTila = "";
+                    delete jonosija.jonosija;
+                });
+            }
+
+        };
+
+        $scope.changeSija = function (jonosija, value) {
+            if (value !== 'HYVAKSYTTAVISSA') {
+                $timeout(function(){
+                    delete jonosija.jonosija;
+                });
+            }
+
+        };
 
 }]);
