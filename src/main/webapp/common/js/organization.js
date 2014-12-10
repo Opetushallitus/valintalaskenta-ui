0d6c67d879303a1c70a227706fe0a0fb2c2737ea
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-12-10 12:24:59 +0200

    VT-1078: eriytetään authorisointia omaan moduuliinsa

---
+++ src/main/webapp/common/js/organization.js
@@ -0,0 +1,28 @@
+
+angular.module('valintalaskenta')
+
+    .service('OrganizationUtility', ['$log', 'OPH_ORG', 'MyRolesModel', '_',
+        function ($log, OPH_ORG, MyRolesModel, _) {
+
+        this.isKorkeakouluOrganization = function () {
+            MyRolesModel.then(function (myroles) {
+                return _.some(myroles, function (role) {
+                    return role.indexOf("APP_VALINTOJENTOTEUTTAMINENKK") > -1;
+                });
+            }, function (error) {
+                $log.error('Käyttäjän roolien hakeminen korkeakoulukäyttöoikeuksien tarkistuksessa epäonnistui');
+            });
+        };
+
+        this.isOphOrganization = function (organization) {
+            return organization.oid === OPH_ORG;
+        };
+
+        this.notKorkeakouluOrganization = function (organization) {
+            return !(!organization.oppilaitosTyyppiUri ||
+            organization.oppilaitosTyyppiUri.indexOf('_41') > -1 ||
+            organization.oppilaitosTyyppiUri.indexOf('_42') > -1 ||
+            organization.oppilaitosTyyppiUri.indexOf('_43') > -1 );
+        };
+
+    }]);
