b42ebee185c76d0300a5bfd2c1c2da9dd1e2ad36
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-12-11 18:36:55 +0200

    sijoitteluntulokset näkymään korjaus

--- src/main/webapp/app/haku/hakukohteet/sijoittelu_tulos/sijoitteluntulos.js
+++ src/main/webapp/app/haku/hakukohteet/sijoittelu_tulos/sijoitteluntulos.js
@@ -291,10 +291,10 @@ app.factory('SijoitteluntulosModel', function ($q, Ilmoitus, Sijoittelu, LatestS
 angular.module('valintalaskenta').
     controller('SijoitteluntulosController', ['$scope', '$modal', '$routeParams', '$window', 'Kirjepohjat', 'Latausikkuna', 'HakukohdeModel',
         'SijoitteluntulosModel', 'OsoitetarratSijoittelussaHyvaksytyille', 'Hyvaksymiskirjeet', 'HakukohteelleJalkiohjauskirjeet',
-        'Jalkiohjauskirjeet', 'SijoitteluXls', 'AuthService', 'HaeDokumenttipalvelusta', 'LocalisationService','HakuModel', 'Ohjausparametrit', 'HakuUtility', '_', '$log', 'Korkeakoulu',
+        'Jalkiohjauskirjeet', 'SijoitteluXls', 'AuthService', 'HaeDokumenttipalvelusta', 'LocalisationService','HakuModel', 'Ohjausparametrit', 'HakuUtility', '_', '$log', 'Korkeakoulu', 'NimiService',
         function ($scope, $modal, $routeParams, $window, Kirjepohjat, Latausikkuna, HakukohdeModel,
                                     SijoitteluntulosModel, OsoitetarratSijoittelussaHyvaksytyille, Hyvaksymiskirjeet, HakukohteelleJalkiohjauskirjeet,
-                                    Jalkiohjauskirjeet, SijoitteluXls, AuthService, HaeDokumenttipalvelusta, LocalisationService, HakuModel, Ohjausparametrit, HakuUtility, _, $log, Korkeakoulu) {
+                                    Jalkiohjauskirjeet, SijoitteluXls, AuthService, HaeDokumenttipalvelusta, LocalisationService, HakuModel, Ohjausparametrit, HakuUtility, _, $log, Korkeakoulu, NimiService) {
     "use strict";
     $scope.hakuOid = $routeParams.hakuOid;
     $scope.HAKEMUS_UI_URL_BASE = HAKEMUS_UI_URL_BASE;
@@ -384,7 +384,9 @@ angular.module('valintalaskenta').
     	} else {
     		tag = $routeParams.hakukohdeOid;
     	}
-    	var langcode = $scope.hakukohdeModel.getKieliCode();
+
+
+    	var langcode = NimiService.getKieliCode($scope.hakukohdeModel.hakukohde);
     	var templateName = "jalkiohjauskirje";
     	var viestintapalveluInstance = $modal.open({
             backdrop: 'static',
@@ -432,7 +434,7 @@ angular.module('valintalaskenta').
     	} else {
     		tag = $routeParams.hakukohdeOid;
     	}
-    	var langcode = $scope.hakukohdeModel.getKieliCode();
+    	var langcode = NimiService.getKieliCode($scope.hakukohdeModel.hakukohde);
     	var templateName = $scope.hakuaVastaavaHyvaksymiskirjeMuotti();
     	var viestintapalveluInstance = $modal.open({
             backdrop: 'static',
@@ -527,7 +529,7 @@ angular.module('valintalaskenta').
     	} else {
     		tag = $routeParams.hakukohdeOid;
     	}
-    	var langcode = $scope.hakukohdeModel.getKieliCode();
+    	var langcode = NimiService.getKieliCode($scope.hakukohdeModel.hakukohde);
     	var templateName = $scope.hakuaVastaavaHyvaksymiskirjeMuotti();
     	var viestintapalveluInstance = $modal.open({
             backdrop: 'static',
