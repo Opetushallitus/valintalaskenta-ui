77bb92969bbe48c93d5d322d881c3d297d7fdaa1
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-04-20 12:42:52 +0300

    yksikkötestit ‘näytä kaikki’ -näkymälle

--- src/main/webapp/test/page/nayta_kaikki.js
+++ src/main/webapp/test/page/nayta_kaikki.js
@@ -1,20 +1,27 @@
+
 function nayta_kaikkiPage() {
   function isLocalhost() {
     return location.host.indexOf('localhost') > -1
   }
 
-  var opiskelijatiedotPage = openPage("/valintalaskenta-ui/app/haku/hakukohteet/pistesyotto/nayta_kaikki/nayta_kaikki.html", function () {
+  var opiskelijatiedotPage = openPage("/valintalaskenta-ui/app/haku/hakukohteet/pistesyotto/nayta_kaikki/index.html#/haku/h/hakukohde/h0", function () {
     return S(".virkailija-table-1").length === 1
   })
 
   var pageFunctions = {
+    search: function() {
+      return S("#search");
+    },
     allStudentsTable: function () {
       return S(".virkailija-table-1").first()
+    },
+    nthNameInTable: function (n) {
+      return S(".virkailija-table-1 tr:nth-child("+n+") td:nth-child(1)").text().trim();
     },
     openPage: function (done) {
       return opiskelijatiedotPage()
         .then(wait.until(function () {
-          var pageReady = pageFunctions.allStudentsTable.length === 1
+          var pageReady = pageFunctions.allStudentsTable().length === 1
           if (pageReady) {
             done()
           }
