998d3e4129d1a08256a277d4e90e548efd3709f9
kjsaila <kalle.saila@gofore.com>
2015-04-22 08:27:14 +0300

    pistesyötön näytä kaikki samaan angular appiin

---
+++ src/main/webapp/app/haku/hakukohteet/pistesyotto/nayta_kaikki/nayta_kaikki.js
@@ -0,0 +1,75 @@
+app.factory('PistesyottoNaytaKaikkiModel', function ($q, HakukohdeAvaimet, HakemusAdditionalData, _) {
+    "use strict";
+
+    if (window.mocksOn) {
+        angular.module('valintalaskenta').requires.push('e2e-mocks');
+    }
+
+    var model;
+    model = new function () {
+
+        this.hakeneet = [];
+        this.avaimet = [];
+
+        this.refresh = function (hakukohdeOid, hakuOid) {
+
+            model.hakeneet.length = 0;
+            model.avaimet.length = 0;
+
+            model.hakuOid = hakuOid;
+            model.hakukohdeOid = hakukohdeOid;
+
+            model.avaimet = HakukohdeAvaimet.get({hakukohdeOid: hakukohdeOid});
+            model.hakeneet = HakemusAdditionalData.get({hakuOid: hakuOid, hakukohdeOid: hakukohdeOid});
+
+        };
+
+        this.refreshIfNeeded = function (hakukohdeOid, hakuOid) {
+
+            if (hakukohdeOid && hakukohdeOid !== model.hakukohdeOid) {
+                model.refresh(hakukohdeOid, hakuOid);
+            }
+
+        };
+
+
+    }();
+
+    return model;
+});
+
+
+angular.module('valintalaskenta').
+    controller('PistesyottoNaytaKaikkiController', ['$scope', '$log', '$routeParams',
+        'PistesyottoNaytaKaikkiModel', 'LocalisationService',
+        function ($scope, $log, $routeParams, PistesyottoNaytaKaikkiModel, LocalisationService) {
+    "use strict";
+
+    $scope.hakukohdeOid = $routeParams.hakukohdeOid;
+    $scope.model = PistesyottoNaytaKaikkiModel;
+    $scope.hakuOid = $routeParams.hakuOid;
+
+
+    PistesyottoNaytaKaikkiModel.refreshIfNeeded($scope.hakukohdeOid, $routeParams.hakuOid);
+
+    $scope.predicate = ['lastName','firstNames'];
+    $scope.osallistumislokaalit = [
+        {value:"hakukohteetpistesyotto.merkitsematta",default_text:"Merkitsem\u00E4tt\u00E4"},
+        {value:"hakukohteetpistesyotto.osallistui",default_text:"Osallistui"},
+        {value:"hakukohteetpistesyotto.eiosallistunut",default_text:"Ei osallistunut"},
+        {value:"hakukohteetpistesyotto.eivaadita",default_text:"Ei vaadita"}
+    ];
+    $scope.lokaali = function(avain) {
+        return _.find($scope.osallistumislokaalit, function(o) { return o.value == avain; }).default_text;
+    };
+    LocalisationService.getTranslationsForArray($scope.osallistumislokaalit);
+    $scope.osallistumistilat = {
+        "MERKITSEMATTA": $scope.lokaali("hakukohteetpistesyotto.merkitsematta"),
+        "OSALLISTUI": $scope.lokaali("hakukohteetpistesyotto.osallistui"),
+        "EI_OSALLISTUNUT": $scope.lokaali("hakukohteetpistesyotto.eiosallistunut"),
+        "EI_VAADITA": $scope.lokaali("hakukohteetpistesyotto.eivaadita")
+    };
+
+    $scope.hakijaFilter = "";
+
+}]);
