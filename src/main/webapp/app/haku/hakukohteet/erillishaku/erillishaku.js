affc8a296adb2fc87850fdcec1f7c96f926a57a6
Jani Rahkola <jani.rahkola@reaktor.com>
2016-03-23 15:35:19 +0200

    Calculate kielistykset at correct time

--- src/main/webapp/app/haku/hakukohteet/erillishaku/erillishaku.js
+++ src/main/webapp/app/haku/hakukohteet/erillishaku/erillishaku.js
@@ -19,6 +19,13 @@
       $scope.valintatuloksentilat = [];
       $scope.korkeakoulu;
 
+      function valintatuloksenTilojenKielistykset(valintatuloksentilat) {
+        return valintatuloksentilat.reduce(function(o, tila) {
+          o[tila.value] = tila.text_prop || tila.default_text;
+          return o;
+        }, {});
+      }
+
       HakuModel.promise.then(function(model) {
         $scope.korkeakoulu = model.korkeakoulu;
         if ($scope.korkeakoulu) {
@@ -40,17 +47,12 @@
             {value:"",default_text:""}
           ];
         }
+        $scope.valintatuloksenTilaKielistys = valintatuloksenTilojenKielistykset($scope.valintatuloksentilat);
         LocalisationService.getTranslationsForArray($scope.valintatuloksentilat).then(function () {
           HakukohdeModel.refreshIfNeeded($routeParams.hakukohdeOid);
-          $scope.valintatuloksenTilaKielistys = $scope.valintatuloksentilat.reduce(function(o, tila) {
-              return o[tila.value] = tila.text_prop || tila.default_text;
-          }, {});
+          $scope.valintatuloksenTilaKielistys = valintatuloksenTilojenKielistykset($scope.valintatuloksentilat);
         });
       });
-
-      $scope.valintatuloksenTilaKielistys = $scope.valintatuloksentilat.reduce(function(o, tila) {
-        return o[tila.value] = tila.default_text;
-      }, {});
 
       $scope.hakemuksentilat = [
         {value:"HYVAKSYTTY",default_text:"Hyv\u00E4ksytty"},
