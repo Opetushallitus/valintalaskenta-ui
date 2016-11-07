189563dad56251c55162b773c6a1ada0ccc700e0
Mikko Apo <mikko.apo@reaktor.fi>
2016-11-07 11:12:10 +0200

    removed valintalaskenta-ui-oph.properties

--- src/main/java/fi/vm/sade/valintalaskenta/config/UrlConfiguration.java
+++ src/main/java/fi/vm/sade/valintalaskenta/config/UrlConfiguration.java
@@ -8,7 +8,6 @@ import java.nio.file.Paths;
 @Configuration
 public class UrlConfiguration extends OphProperties {
     public UrlConfiguration() {
-        addFiles("/valintalaskenta-ui-oph.properties");
         addOptionalFiles(Paths.get(System.getProperties().getProperty("user.home"), "/oph-configuration/common.properties").toString());
     }
 }
