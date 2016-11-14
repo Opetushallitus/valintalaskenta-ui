e506c818ad34a7187746024c543c5b6f546a5507
August Kilponen <august.kilponen@gmail.com>
2016-11-14 15:28:30 +0200

    Päivitetty phantomjs versioon 2.1.7

--- src/test/java/fi/vm/sade/valintaperusteet/mocha/MochaTestRunnerTest.java
+++ src/test/java/fi/vm/sade/valintaperusteet/mocha/MochaTestRunnerTest.java
@@ -13,7 +13,7 @@ public class MochaTestRunnerTest {
     @Test
     public void mochaTest() throws Exception {
         JettyTestRunner.start();
-        String[] cmd = {"node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:" + JettyTestRunner.PORT + "/valintalaskenta-ui/test/runner.html"};
+        String[] cmd = {"node_modules/phantomjs-prebuilt/bin/phantomjs", "node_modules/mocha-phantomjs-core/mocha-phantomjs-core.js", "http://localhost:" + JettyTestRunner.PORT + "/valintalaskenta-ui/test/runner.html", "spec"};
 
         ProcessBuilder pb = new ProcessBuilder(cmd);
         pb.inheritIO();
