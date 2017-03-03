1b9cf8a9d9d549d61a7bd9bd27936584729ea750
Heikki Honkanen <heikki.honkanen@gofore.com>
2017-03-03 14:21:55 +0200

    Add feature flag to read valinnantulokset from valintarekisteri.

--- src/main/webapp/common/js/valinnantulokset.js
+++ src/main/webapp/common/js/valinnantulokset.js
@@ -8,7 +8,7 @@ angular.module('valintalaskenta')
 
   .factory('Valinnantulokset', [function () {
     var valinnantulokset = {},
-      useVtsData;
+      useVtsData = READ_FROM_VALINTAREKISTERI;
 
     valinnantulokset.compareOldAndNewVtsResponse = function(oldVtsValintatapajono, newVtsValinnantulokset, checkKeys) {
       compareValintatapajonoValinnantulokset(oldVtsValintatapajono, newVtsValinnantulokset);
