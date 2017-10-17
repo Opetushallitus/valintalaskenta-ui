bd705158b6a35d046f48fd25a4806d84761f32de
Joni Mäkelä <joni.makela@gmail.com>
2017-10-17 10:21:17 +0300

    Renamed test classes so that navigating to the correct class is easier...

--- src/test/java/fi/vm/sade/valintaperusteet/mocha/ValintalaskentaUIMochaTestRunner.java
+++ src/test/java/fi/vm/sade/valintaperusteet/mocha/ValintalaskentaUIMochaTestRunner.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class MochaTestRunnerTest {
+public class ValintalaskentaUIMochaTestRunner {
 
     @Test
     public void mochaTest() throws Exception {
