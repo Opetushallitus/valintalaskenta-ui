70ba971b58201f4f047a84bfa5f131087ef136f0
Joni Mäkelä <joni.makela@gmail.com>
2017-12-20 15:57:34 +0200

    Pass window.url to a scope function and use that in html

--- src/main/webapp/app/jono/hallinta.js
+++ src/main/webapp/app/jono/hallinta.js
@@ -54,6 +54,8 @@ angular
     // Jobs being processed are subscribed to SSE endpoint for a soft real-time updates
     $scope.sseTrackedJobs = {};
 
+    $scope.url = $window.url;
+
     $scope.isShown = function(elem, idx, arr) {
       return $scope.stateFilters[elem.tila] || _.every($scope.stateFilters, function(state) {return !state});
     };
