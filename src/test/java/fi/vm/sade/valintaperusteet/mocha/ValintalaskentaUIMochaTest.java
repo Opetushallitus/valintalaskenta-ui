24ec7a3f6a16a44dfd7e8d556948be91f9c299d0
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-03-12 15:08:42 +0200

    Rename tests
    This way Maven will run them.

--- src/test/java/fi/vm/sade/valintaperusteet/mocha/ValintalaskentaUIMochaTest.java
+++ src/test/java/fi/vm/sade/valintaperusteet/mocha/ValintalaskentaUIMochaTest.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class ValintalaskentaUIMochaTestRunner {
+public class ValintalaskentaUIMochaTest {
 
     @Test
     public void mochaTest() throws Exception {
