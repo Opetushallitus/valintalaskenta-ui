aaf6a79574f84c0cb31cce68426348b0246964ae
Jani Rahkola <jani.rahkola@reaktor.com>
2018-05-31 15:58:44 +0300

    Refactor henkilo.js

--- src/main/webapp/app/haku/henkilot/henkilo.js
+++ src/main/webapp/app/haku/henkilot/henkilo.js
@@ -1,101 +1,96 @@
 var app = angular.module('valintalaskenta');
 app.factory('HenkiloModel', function ($resource, $q, $routeParams, Henkilot) {
-    "use strict";
-
-    function getName(hakemus) {
-        return hakemus.lastName + ", " + hakemus.firstNames;
+    function getHakuAppHakemukset(hakuOid, start, n, q) {
+        return Henkilot.query({
+            appState: ["ACTIVE", "INCOMPLETE"],
+            asId: hakuOid,
+            start: start,
+            rows: n,
+            q: q
+        }).$promise.then(function(result) {
+            return {
+                hakemukset: result.results.map(function(h) {
+                    return {
+                        oid: h.oid,
+                        name: h.lastName + ", " + h.firstNames
+                    };
+                }),
+                totalCount: result.totalCount
+            };
+        });
     }
 
-    function getCount() {
-        if (this.hakemukset === undefined) {
-            return 0;
-        }
-        return this.hakemukset.length;
-    }
-
-    function getTotalCount() {
-        return this.totalCount;
-    }
-
-    function getHakemukset() {
-        return this.hakemukset;
-    }
-
-    function getNextPage() {
-        var startIndex = this.getCount();
-        var lastTotalCount = this.getTotalCount();
-        var notLastPage = startIndex < lastTotalCount; //this.totalCount < (this.pages + 1)*this.pageSize;
-        if (notLastPage) {
-            var self = this;
-            if (modelInterface.readyToQueryForNextPage) {
-                modelInterface.readyToQueryForNextPage = false;
-
-                Henkilot.query({appState: ["ACTIVE", "INCOMPLETE"], asId: $routeParams.hakuOid, start: startIndex, rows: this.pageSize, q: this.lastSearch }, function (result) {
-                    if (startIndex !== self.getCount()) {
-                        modelInterface.readyToQueryForNextPage = true;
-                    } else {
-                        self.hakemukset = self.hakemukset.concat(result.results);
-                        if (self.getTotalCount() !== result.totalCount) {
-                            // palvelimen tietomalli on paivittynyt joten koko lista on ladattava uudestaan!
-                            // ... tai vaihtoehtoisesti kayttajalle naytetaan epasynkassa olevaa listaa!!!!!
-                            self.lastSearch = null;
-                            self.refresh();
-                            modelInterface.readyToQueryForNextPage = true;
-                        }
-                    }
-                    modelInterface.readyToQueryForNextPage = true;
-                });
+    function addToHakemukset(model) {
+        var searchWordShould = model.searchWord;
+        return function(result) {
+            if (model.searchWord === searchWordShould) {
+                if (model.totalCount === result.totalCount) {
+                    model.hakemukset = model.hakemukset.concat(result.hakemukset);
+                    return true;
+                } else {
+                    return model.refresh().then(function () {
+                        return false;
+                    });
+                }
+            } else {
+                return false;
             }
         }
     }
 
-    function refresh() {
-        // should also clear all paging information
-        var word = $.trim(this.searchWord);
+    function setHakemukset(model) {
+        var searchWordShould = model.searchWord;
+        return function(result) {
+            if (model.searchWord === searchWordShould) {
+                model.hakemukset = result.hakemukset;
+                model.totalCount = result.totalCount;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
-        this.lastSearch = word;
+    function getNextPage() {
         var self = this;
+        if (this.readyToQueryForNextPage) {
+            this.readyToQueryForNextPage = false;
+            getHakuAppHakemukset(this.hakuOid, this.hakemukset.length, this.pageSize, $.trim(this.searchWord))
+                .then(addToHakemukset(this))
+                .then(function () {
+                    self.readyToQueryForNextPage = true;
+                });
+        }
+    }
 
-        Henkilot.query({appState: ["ACTIVE", "INCOMPLETE"], asId: $routeParams.hakuOid, start: 0, rows: this.pageSize, q: word }, function (result) {
-            self.hakemukset = result.results;
-            self.totalCount = result.totalCount;
-        });
-
+    function refresh() {
+        var self = this;
+        this.hakuOid = this.hakuOid || $routeParams.hakuOid;
+        this.readyToQueryForNextPage = false;
+        getHakuAppHakemukset(this.hakuOid, 0, this.pageSize, $.trim(this.searchWord))
+            .then(setHakemukset(this))
+            .then(function() { self.readyToQueryForNextPage = true; });
     }
 
     function refreshIfNeeded(hakuOid) {
         if (this.hakuOid !== hakuOid) {
             this.hakuOid = hakuOid;
-            this.searchWord = "";
-            this.lastSearch = "";
-            var self = this;
-            Henkilot.query({appState: ["ACTIVE", "INCOMPLETE"], asId: $routeParams.hakuOid, start: 0, rows: this.pageSize, q: this.lastSearch }, function (result) {
-                self.hakemukset = result.results;
-                self.totalCount = result.totalCount;
-            });
+            this.refresh();
         }
     }
 
-    var modelInterface = {
+    return {
         hakemukset: [],
         totalCount: 0,
         pageSize: 30,
         searchWord: "",
-        lastSearch: "",
         readyToQueryForNextPage: true,
         hakuOid: null,
 
         refresh: refresh,
         refreshIfNeeded: refreshIfNeeded,
-        getTotalCount: getTotalCount,
-        getHakemukset: getHakemukset,
-        getNextPage: getNextPage,
-        getCount: getCount,
-        getName: getName
-
+        getNextPage: getNextPage
     };
-
-    return modelInterface;
 });
 
 angular.module('valintalaskenta').
@@ -103,6 +98,7 @@ angular.module('valintalaskenta').
         function ($scope, $location, $routeParams, HenkiloModel) {
     "use strict";
 
+    var debounceTimer = null;
     $scope.model = HenkiloModel;
     $scope.model.refreshIfNeeded($routeParams.hakuOid);
     $scope.hakemusOid = $routeParams.hakemusOid;
@@ -111,24 +107,14 @@ angular.module('valintalaskenta').
         $location.hash($routeParams.scrollTo);
     }
 
-    $scope.$watch('model.searchWord', debounce(function () {
-        HenkiloModel.refresh();
-    }, 500));
+    $scope.$watch('model.searchWord', function() {
+        if (debounceTimer) {
+            clearTimeout(debounceTimer);
+        }
+        debounceTimer = setTimeout($scope.model.refresh.bind($scope.model), 500);
+    });
 
-    function debounce(fn, delay) {
-        var timer = null;
-        return function () {
-            var context = this, args = arguments;
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                fn.apply(context, args);
-            }, delay);
-        };
-    }
-
-    $scope.lazyLoading = function () {
-        $scope.model.getNextPage();
-    };
+    $scope.lazyLoading = $scope.model.getNextPage.bind($scope.model);
 
     $scope.toggleHenkiloittainVisible = function () {
         $scope.henkiloittainVisible = !$scope.henkiloittainVisible;
@@ -137,5 +123,4 @@ angular.module('valintalaskenta').
     $scope.showHakemus = function (hakemus) {
         $location.path('/haku/' + $routeParams.hakuOid + '/henkiloittain/' + hakemus.oid + "/henkilotiedot/id_" + hakemus.oid);
     };
-
 }]);
