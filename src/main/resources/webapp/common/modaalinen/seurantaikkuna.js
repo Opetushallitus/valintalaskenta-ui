dbfaba62af9eb412c9b288cdf332de2f34859f55
Santeri Korri <santeri.korri@knowit.fi>
2024-05-27 09:48:54 +0300

    OK-478 Siirretty valintalaskennan seurannan polut osoittamaan valintalaskentaan

--- src/main/resources/webapp/common/modaalinen/seurantaikkuna.js
+++ src/main/resources/webapp/common/modaalinen/seurantaikkuna.js
@@ -168,7 +168,9 @@ function SeurantaIkkunaCtrl(
     )
   }
   $scope.vieJsoniksi = function () {
-    $window.open(window.url('seuranta-service.seuranta.lataa', $scope.uuid))
+    $window.open(
+      window.url('valintalaskenta-laskenta-service.seuranta.lataa', $scope.uuid)
+    )
   }
 
   var update = function () {
